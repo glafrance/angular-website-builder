6d81a899ad60cab9a06f21c2f050c3d97aab849c
Gregory Lafrance <greglafrance007@gmail.com>
2023-09-06 12:48:38 -0400

    progress creating website build page

--- client/angular/angular-website-builder/src/app/components/pages/build-websites/build-websites.component.ts
+++ client/angular/angular-website-builder/src/app/components/pages/build-websites/build-websites.component.ts
@@ -6,5 +6,30 @@ import { Component } from "@angular/core";
   styleUrls: ["./build-websites.component.scss"]
 })
 export class BuildWebsitesComponent {
-  
+  showLayouts: boolean = false;
+
+  layoutIcons: any = [
+    "../../../assets/images/layout-icons/single.png",
+    "../../../assets/images/layout-icons/two-columns.png",
+    "../../../assets/images/layout-icons/two-rows.png",
+    "../../../assets/images/layout-icons/three-columns.png",
+    "../../../assets/images/layout-icons/three-rows.png",
+    "../../../assets/images/layout-icons/four-columns.png",
+    "../../../assets/images/layout-icons/four-rows.png",
+    "../../../assets/images/layout-icons/four-containers.png",
+    "../../../assets/images/layout-icons/header-footer.png",
+    "../../../assets/images/layout-icons/header-footer-two-columns.png",
+    "../../../assets/images/layout-icons/header-footer-two-rows.png",
+    "../../../assets/images/layout-icons/header-footer-three-columns.png",
+    "../../../assets/images/layout-icons/header-footer-three-rows.png",
+    "../../../assets/images/layout-icons/header-footer-four-containers.png"
+  ];
+
+  onShowLayouts() {
+    this.showLayouts = true;
+  }
+
+  onHideLayouts() {
+    this.showLayouts = false;
+  }
 }
